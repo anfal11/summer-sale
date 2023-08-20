bfd0a07fedfc950711bccd81403d4fa7a958d1fb
anfal11 <anfal15-14303@diu.edu.bd>
2023-08-20 22:24:23 +0600

    finally apply button works

--- JS/script.js
+++ JS/script.js
@@ -3,51 +3,54 @@ const discount = document.getElementById("discount");
 const totalCurrentPrice = document.getElementById("total-current-price");
 
 let total = 0;
+let counter = 1;
 function handleClick(data) {
+  const element = data.childNodes[7].childNodes[1].innerText;
+  const li = document.createElement("li");
+  li.innerText = `${counter}. ${element}`;
+  counter++;
+  li.style.listStyleType = "none";
+  li.style.order = "1";
+  li.style.fontSize = "1.25rem";
+  li.style.fontWeight = "500";
+  li.style.marginTop = "0.25rem";
+  li.style.margin = "1rem";
+  const div = document.getElementById("price-send");
+  div.appendChild(li);
 
-    const element = data.childNodes[7].childNodes[1].innerText;
-    const li = document.createElement("li");
-    li.innerText = element;
-    li.style.fontSize = "1.25rem";
-    li.style.fontWeight = "500";
-    li.style.marginTop = "0.25rem";
-    li.style.margin = "1rem";
-    const div = document.getElementById("price-send");
-    div.appendChild(li);
+  const price = parseFloat(
+    data.childNodes[7].childNodes[3].innerText.split(" ")[0]
+  );
 
-    const price = parseFloat(data.childNodes[7].childNodes[3].innerText.split(" ")[0]);
-    
-    total += price;
-    totalPreviousPrice.innerText = total.toFixed(2) + 'TK';
+  total += price;
+  totalPreviousPrice.innerText = total.toFixed(2) + "TK";
 
-    if (total > 0){
-        const purchaseBtn = document.getElementById("purchase-btn");
-        purchaseBtn.disabled = false;
-        purchaseBtn.style.backgroundColor = "#E527B2";
-        purchaseBtn.style.color = "#fff";
-    } 
-    
-    if (total >= 200){
-        const btnApply = document.getElementById("btn-apply");
-        btnApply.disabled = false;
-        btnApply.style.backgroundColor = "#E527B2";
-        btnApply.style.color = "#fff"; 
+  if (total > 0) {
+    const purchaseBtn = document.getElementById("purchase-btn");
+    purchaseBtn.disabled = false;
+    purchaseBtn.style.backgroundColor = "#E527B2";
+    purchaseBtn.style.color = "#fff";
+    purchaseBtn.style.cursor = "pointer";
+  }
 
-        btnApply.addEventListener("click", function(){
-            console.log(total);
-            console.log("clicked");
-            const couponCode = document.getElementById("coupon-code");
-            if (couponCode.value === "SELL20"){
-                const discountPrice = total * 0.2;
-                discount.innerText = discountPrice.toFixed(2) + 'TK';
-                totalCurrentPrice.innerText = (total - discountPrice).toFixed(2) + 'TK';
-            }
-            else{
-                alert("Invalid Coupon Code");
-            }
-        })  
-     
-        
-    }
+  if (total >= 200) {
+    const btnApply = document.getElementById("btn-apply");
+    btnApply.disabled = false;
+    btnApply.style.backgroundColor = "#E527B2";
+    btnApply.style.color = "#fff";
+    btnApply.style.cursor = "pointer";
 
+    btnApply.addEventListener("click", function () {
+      console.log(total);
+      console.log("clicked");
+      const couponCode = document.getElementById("coupon-code");
+      if (couponCode.value.toUpperCase() === "SELL200") {
+        const discountPrice = total * 0.2;
+        discount.innerText = discountPrice.toFixed(2) + "TK";
+        totalCurrentPrice.innerText = (total - discountPrice).toFixed(2) + "TK";
+      } else {
+        alert("Invalid Coupon Code");
+      }
+    });
+  }
 }
